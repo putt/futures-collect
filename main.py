fdf32458855c8f098bb63d5958db0ac0e9eba7ae
putt <putt@users.noreply.github.com>
2019-08-12 20:43:14 +0800

    使用新的新浪数据源

--- main.py
+++ main.py
@@ -44,32 +44,42 @@ def volumeIncrease(pv_data, price, volume):
 
 if __name__=="__main__":
     # 'http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesDailyKLine?symbol=M1701'
-    base_url = 'http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesMiniKLine5m?symbol='
+#    base_url = 'http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesMiniKLine5m?symbol='
+    base_url = 'https://stock2.finance.sina.com.cn/futures/api/jsonp.php/var%20_{}_{}=/InnerFuturesNewService.getFewMinLine?symbol={}&type=5'
     pvplot = PriceVolumePlotter()
     # for symbol in contracts:
     while 1:
-        hour = datetime.now().hour
-#        if (hour>8 and hour < 15) or hour >20:
+        now = datetime.now()
+        hour = now.hour
+       # if (hour>8 and hour < 15) or hour >20:
         if 1:
             for symbol in contracts:
                 inst = dealZhengZhou(symbol)
                 # print (inst, symbol)
-                url = base_url + inst
+                url = base_url.format(inst, now.timestamp(), inst)
                 bar_table = symbol + '_5MBar'
                 vol_table = symbol + '_VolDistribution'
-                print ('url = ' + url)
-
-                results = json.load(urllib.request.urlopen(url))
+                print (inst)
+                
+                results = urllib.request.urlopen(url).read()
+                remote_data = pd.read_json(results[results.find(b'(')+1: results.find(b')')]).set_index('d')
                 c = conn.cursor()
                 c.execute("SELECT TradingTime from %s DESC LIMIT 1" %(bar_table))
                 last = c.fetchone()
+                
+                if not last:
+                    new_data = remote_data
+                else:
+                    new_data = remote_data.loc[]
 
                 bar_data = []
                 vol_data = []
                 pv_data = {}
                 for r in results:
+                    # r = ["2019-02-28 23:00:00","2631.000","2631.000","2630.000","2631.000","74"]
+                    # datetime, open, high, low, close, volume
                     if not last or r[0] > last[0]:
-                        bar_data.append((symbol, float(r[1]), float(r[2]), float(r[3]), float(r[4]), int(r[5]), r[0]))
+                        bar_data.append((symbol, float(r[1]), float(r[2]), float(r[3]), float(r[4]), int(r[5]), r[0])) # symbol, o, h, l, c, v, datetime
                         vol_data.append((float(r[1]), float(r[5])/4))
                         vol_data.append((float(r[2]), float(r[5])/4))
                         vol_data.append((float(r[3]), float(r[5])/4))
@@ -77,18 +87,18 @@ if __name__=="__main__":
                 conn.executemany("INSERT INTO %s (inst, open, high, low, close, volume, TradingTime) VALUES (?,?,?,?,?,?,?)"%bar_table ,bar_data)
                 conn.executemany("INSERT INTO %s (price, volume) VALUES (?,?)"%vol_table , vol_data)
                 conn.commit()
-                # a = np.array(vol_data)
-                # print (a)
-                # b= a[a[:,0].argsort()]
-                # print (b)
-                # prices, volumes = zip(*vol_data)
 
                 c.execute('SELECT price, sum(volume) from %s group by price order by price'%vol_table)
               
-                prices, volumes = zip(*c.fetchall())              
-                pvplot.plot(symbol, prices, volumes)
-            
+                prices, volumes = zip(*c.fetchall())   
+                if bar_data:
+                    current =  (bar_data[0][6], bar_data[0][4])
+                else:
+                    current = (results[0][0], results[0][4])
+                pvplot.plot(symbol,current , prices, volumes)
         else:
-            time.sleep(300000)
-        time.sleep(60000)
+            print("Waiting for trading time.")
+            time.sleep(300)
+        print ('Waiting next period.')
+        time.sleep(100)
         
--- pvplot.py
+++ pvplot.py
@@ -1,50 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.interpolate as ip
-import seaborn as sns
+
 
 
 
 class PriceVolumePlotter():
-	def __init__(self):
-		
-		plt.rcParams["figure.figsize"] = 6,14
-		self.fig, self.ax = plt.subplots()
+    def __init__(self):
+        
+        plt.rcParams["figure.figsize"] = 6,14
+        self.fig, self.ax = plt.subplots()
 
 
-	def plot(self, symbol, prices, volumes):
-		# the histogram of the data
-		# print (volumes)
-		# points = np.mgrid[min(prices):max(prices):100j]
+    def plot(self, symbol, current, prices, volumes):
+        # the histogram of the data
+        # print (volumes)
+        # points = np.mgrid[min(prices):max(prices):100j]
 
-		# grid_z0 = griddata(prices, volumes, points, method='nearest')
-		# grid_z1 = griddata(prices, volumes, points, method='linear')
-		# grid_z2 = griddata(prices, volumes, points, method='cubic')
-		self.ax.clear()
+        # grid_z0 = griddata(prices, volumes, points, method='nearest')
+        # grid_z1 = griddata(prices, volumes, points, method='linear')
+        # grid_z2 = griddata(prices, volumes, points, method='cubic')
+        self.ax.clear()
+       
 
-		maxprice = max(prices)
-		minprice = min(prices)
-		size = 300
-#		xnew = np.linspace(minprice,maxprice,size) #300 represents number of points to make between T.min and T.max
-#		 
-#		f = ip.interp1d(prices,volumes,kind='nearest')
-		# print (xnew)
-		# print (f(xnew))
-#		width = (maxprice - minprice)/size
-#		self.ax.bar(xnew,f(xnew), align='center', width=width)
-		self.ax.hist(prices, bins=100, weights=volumes, orientation='horizontal')
-		# self.ax.bar([114540., 117685. ,120830. ,123975. ,127120.], [ 3842.5, 15145.61170213 ,55955.84761905 ,14572.375, 15750])
+        maxprice = max(prices)
+        minprice = min(prices)
+        size =80
+#        xnew = np.linspace(minprice,maxprice,size) #300 represents number of points to make between T.min and T.max
+#         
+#        f = ip.interp1d(prices,volumes,kind='nearest')
+        # print (xnew)
+        # print (f(xnew))
+#        width = (maxprice - minprice)/size
+#        self.ax.bar(xnew,f(xnew), align='center', width=width)
+        self.ax.hist(prices, bins=size, weights=volumes, orientation='horizontal', histtype='stepfilled')
+        
+        # self.ax.bar([114540., 117685. ,120830. ,123975. ,127120.], [ 3842.5, 15145.61170213 ,55955.84761905 ,14572.375, 15750])
 
-		# width = (maxprice - minprice)/len(prices)
-		# self.ax.bar(prices, volumes, width=width)
-		self.ax.set_title(symbol)
-		# self.ax.invert_yaxis()
+        # width = (maxprice - minprice)/len(prices)
+        # self.ax.bar(prices, volumes, width=width)
+        self.ax.set_title('{} {}'.format(symbol,current[0]))
+        # self.ax.invert_yaxis()
 
-		# Tweak spacing to prevent clipping of ylabel
-		# self.fig.tight_layout()
-		self.fig.savefig('figures/'+symbol+ ".png") 
-		# self.fig.clear()
+        # Tweak spacing to prevent clipping of ylabel
+        # self.fig.tight_layout()
+        plt.axhline(y=current[1], color='r')
+        self.fig.savefig('figures/'+symbol+ ".png") 
+        # self.fig.clear()
 
 
 if __name__=="__main__":
-	PriceVolumePlotter().plot('aaa',[114540., 117685. ,120830. ,123975. ,127120.], [ 3842.5, 15145.61170213 ,55955.84761905 ,14572.375, 15750])
+    PriceVolumePlotter().plot('aaa',[114540., 117685. ,120830. ,123975. ,127120.], [ 3842.5, 15145.61170213 ,55955.84761905 ,14572.375, 15750])
